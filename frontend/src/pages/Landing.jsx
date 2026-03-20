189b4ea3f0b4a40a457a0902d4f325f18475fb10
vidhirawat10 <vidhirawat54@gmail.com>
2026-03-20 21:54:59 +0530

    updates on landing pg

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,24 @@
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
+import ScaleSection from "../components/sections/ScaleSection"
+import Features from "../components/sections/Features"
 
 const Landing = () => {
   return (
-    <>
+    <div className="landing">
+
+      {/* NAVBAR */}
       <Navbar />
+
+      {/* HERO SECTION */}
       <Hero />
-    </>
+
+      {/* SCALE / LIGHTNING SECTION */}
+      <ScaleSection />
+
+      <Features />
+
+    </div>
   )
 }
 
